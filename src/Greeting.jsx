8493bd703fd0069320dcae006df777b89dddd246
ThatBaldDude4 <taylorv0004@gmail.com>
2026-06-01 10:10:58 -0500

    create test components

---
+++ src/Greeting.jsx
@@ -0,0 +1,10 @@
+function Greeting() {
+    return (
+        <div>
+            <h1>HELLO THERE PARTNER</h1>
+            <p>This is one of my first react components</p>
+        </div>
+    )
+}
+
+export default Greeting
