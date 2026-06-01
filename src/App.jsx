f26c215477d5741e01a45f6ad62626b077a38ad2
ThatBaldDude4 <127453013+ThatBaldDude4@users.noreply.github.com>
2026-06-01 20:14:50 +0000

    add App test component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import { useState } from 'react'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <p>
-        Hello World
-      </p>
-    </>
+    <div>
+      <h1>Test title</h1>
+      <svg>
+        <circle cx="25" cy="75" r="20" stroke="green" strokeWidth="2" />
+      </svg>
+      <form>
+        <input type="text" />
+      </form>
+    </div>
   )
 }
 
